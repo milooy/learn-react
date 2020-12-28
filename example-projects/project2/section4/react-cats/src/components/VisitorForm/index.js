d212039f50e51e6ff3d81e46b10e53a8d5b672fc
Yurim Jin <jayjinjay@gmail.com>
2020-12-28 23:17:49 +0900

    controlled form

--- example-projects/project2/section4/react-cats/src/components/VisitorForm/index.js
+++ example-projects/project2/section4/react-cats/src/components/VisitorForm/index.js
@@ -2,7 +2,22 @@ import React from "react";
 import "./VisitorForm.css";
 
 export default class VisitorForm extends React.Component {
+  state = { email: "", contents: "" };
+
+  updateEmail(e) {
+    this.setState({ email: e.target.value });
+  }
+
+  updateContents(e) {
+    this.setState({ contents: e.target.value });
+  }
+
+  resetForm(e) {
+    this.setState({ email: "", contents: "" });
+  }
+
   render() {
+    const { email, contents } = this.state;
     return (
       <>
         <h2 style={{ marginTop: 50 }}>방명록</h2>
@@ -10,15 +25,30 @@ export default class VisitorForm extends React.Component {
           className="visitor-form"
           onSubmit={(e) => {
             e.preventDefault();
-            console.log(e.target.elements.email.value);
-            console.log(e.target.elements.contents.value);
+            alert(`email: ${email}, contents: ${contents}`);
+            this.resetForm();
           }}
         >
           <label htmlFor="email">이메일</label>
-          <input type="email" name="email" />
+          <input
+            value={email}
+            onChange={this.updateEmail.bind(this)}
+            type="email"
+            name="email"
+          />
           <label htmlFor="visitor-form-contents">내용</label>
-          <textarea name="contents" id="visitor-form-contents" />
-          <button>보내기</button>
+          <textarea
+            onChange={this.updateContents.bind(this)}
+            name="contents"
+            id="visitor-form-contents"
+            value={contents}
+          />
+          <div>
+            <button onClick={this.resetForm.bind(this)} type="reset">
+              내용 삭제
+            </button>
+            <button type="submit">보내기</button>
+          </div>
         </form>
       </>
     );
